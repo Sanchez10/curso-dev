27084f422c4a65ada28bfcd0f5e3becc02f678b5
Joao Sanchez <joao.sanchez01@gmail.com>
2025-01-06 23:50:05 -0300

    Adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # curso-dev
+
 Repositório para salvar os códigos e atividades relacionadas ao curso.dev.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>João Pedro Sanchez Gabriel ama a Satie Tamashiro! ❤️</h1>
+  return <h1>João Pedro Sanchez Gabriel ama a Satie Tamashiro! ❤️</h1>;
 }
 
 export default Home;
